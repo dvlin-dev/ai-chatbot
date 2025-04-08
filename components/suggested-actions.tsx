b6e1dcabfb50529297931e699c6cb48902b9127c
zhangbaolin <zhangbaolin.work@foxmail.com>
2025-04-08 18:57:02 +0800

    feat: 更新 ESLint 配置，修改页面标题和描述，调整聊天组件导入类型，更新建议的操作内容

--- components/suggested-actions.tsx
+++ components/suggested-actions.tsx
@@ -13,24 +13,12 @@ interface SuggestedActionsProps {
 function PureSuggestedActions({ chatId, append }: SuggestedActionsProps) {
   const suggestedActions = [
     {
-      title: 'What are the advantages',
-      label: 'of using Next.js?',
-      action: 'What are the advantages of using Next.js?',
+      title: 'What is wegic?',
+      action: 'What is wegic?',
     },
     {
-      title: 'Write code to',
-      label: `demonstrate djikstra's algorithm`,
-      action: `Write code to demonstrate djikstra's algorithm`,
-    },
-    {
-      title: 'Help me write an essay',
-      label: `about silicon valley`,
-      action: `Help me write an essay about silicon valley`,
-    },
-    {
-      title: 'What is the weather',
-      label: 'in San Francisco?',
-      action: 'What is the weather in San Francisco?',
+      title: 'How is the weather in Beijing today?',
+      action: 'How is the weather in Beijing today?',
     },
   ];
 
